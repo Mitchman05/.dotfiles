53ed2dc34c2e5b423d9514d258c5e1125edeb883
mitchman05 <masterthefire95@gmail.com>
2024-10-22 21:58:18 +1100

    Added fish config

---
+++ fish/.config/fish/functions/aliases.fish
@@ -0,0 +1,12 @@
+# Easy aliases
+alias neofetch = "fastfetch"
+alias ls = "lsd"
+
+# More complicated ones
+function cat --wraps=bat --description 'alias cat=bat'
+	bat $argv
+end
+
+function ls --wraps=lsd --description 'alias ls=lsd'
+	lsd $argv
+end
