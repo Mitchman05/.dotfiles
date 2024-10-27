d1af4080af79ad7d0137aae1e16fe10662380f19
mitchman05 <masterthefire95@gmail.com>
2024-10-27 21:31:17 +1100

    Updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 cp hypr/.config/hypr/hyprland/monitor-template.conf hypr/.config/hypr/hyprland/monitor.conf
 touch hypr/.config/hypr/hyprland/local.conf
 
+# Install yay
+pacman -S yay
+
+# Install dependencies
+yay -S --needed - < ./dependencies/*.txt
+
+# Stow dotfiles directories
+stow --adopt ags fastfetch fcitx5 fish gammastep kitty misc nvim rofi waybar
 
 # Add sddm themes
 mkdir -p /usr/share/sddm
-stow -t /usr/share/sddm sddm
+stow --adopt -t /usr/share/sddm sddm
+
+# Force dotfiles to revert to repo
+git restore
