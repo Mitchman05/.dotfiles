fc7cfd6923c5d79fccc76e85d50c239f02f2f8d3
mitchman05 <masterthefire95@gmail.com>
2024-10-27 20:58:44 +1100

    Updated dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 # Create local config files for hyprland
 cp hypr/.config/hypr/hyprland/monitor-template.conf hypr/.config/hypr/hyprland/monitor.conf
 touch hypr/.config/hypr/hyprland/local.conf
+
+
+# Add sddm themes
+mkdir -p /usr/share/sddm
+stow -t /usr/share/sddm sddm
