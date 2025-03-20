dcbd389c86585dab5169156c3d031fdb8fdfa1b4
mitchman05 <masterthefire95@gmail.com>
2025-03-20 18:31:44 +1100

    Started reworking neovim configs, got completions working

---
+++ nvim/.config/nvim/completions.lua
@@ -0,0 +1,43 @@
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<CR>'] = function(fallback)
+		if cmp.visible() then
+			cmp.confirm()
+		else
+			fallback()
+		end
+	end,
+	['<TAB>'] = function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		else
+			fallback()
+		end
+	end,
+	['<S-TAB>'] = function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		else
+			fallback()
+		end
+	end,
+    ['<ESC>'] = function(fallback)
+		if cmp.visible() then
+			cmp.close()
+		else
+			fallback()
+		end
+	end,
+  },
+})
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,81 +17,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
-require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-    },
-    color_overrides = {},
-    default_integrations = true,
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        neotree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-		native_lsp = {
-			enabled = true,
-			virtual_text = {
-				errors = { "italic" },
-				hints = { "italic" },
-				warnings = { "italic" },
-				information = { "italic" },
-				ok = { "italic" },
-			},
-			underlines = {
-				errors = { "underline" },
-				hints = { "underline" },
-				warnings = { "underline" },
-				information = { "underline" },
-				ok = { "underline" },
-			},
-			inlay_hints = {
-				background = false,
-			},
-		},
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-    custom_highlights = function(colors)
-		return {
-			LineNr = { fg = '#7f849c' },
-		}
-    end
-})
+require("completions")
 
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
+require("themes")
 
 vim.lsp.inlay_hint.enable(true)
 
@@ -101,24 +29,9 @@ vim.wo.relativenumber = true
 vim.opt["tabstop"] = 4
 vim.opt["shiftwidth"] = 4
 
-local cmp = require('cmp')
-
-cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  snippet = {
-    expand = function(args)
-      -- You need Neovim v0.10 to use vim.snippet
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({}),
-})
-
 require('mini.surround').setup()
 require('mini.comment').setup()
 require('mini.surround').setup()
 require('mini.pairs').setup()
 
-require'lspconfig'.clangd.setup{}
+require('lspconfig').clangd.setup{}
