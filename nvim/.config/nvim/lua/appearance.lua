16611b3fc2752e729f482d6fe6d6d4878aa224ec
Mitchman05 <masterthefire95@gmail.com>
2025-04-11 23:14:58 +1000

    Updated appearance config

--- nvim/.config/nvim/lua/appearance.lua
+++ nvim/.config/nvim/lua/appearance.lua
@@ -1,11 +1,12 @@
 vim.lsp.inlay_hint = true
 vim.wo.number = true
 vim.wo.relativenumber = true
+vim.wo.signcolumn = "yes"
 
 vim.diagnostic.config({ virtual_text = true })
-
-vim.fn.matchadd("Error", '\\%>80c')
 
 require("ibl").setup {
 	scope = {enabled = false}
 }
+
+require("gitsigns").setup()
