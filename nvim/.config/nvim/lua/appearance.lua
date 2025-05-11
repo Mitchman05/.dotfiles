69f333a6f481fd522df4ed0043804cb01ae5a691
Mitchman05 <masterthefire95@gmail.com>
2025-05-11 17:53:10 +1000

    Made vimtex not use conceal

--- nvim/.config/nvim/lua/appearance.lua
+++ nvim/.config/nvim/lua/appearance.lua
@@ -2,6 +2,7 @@ vim.lsp.inlay_hint = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.signcolumn = "yes"
+vim.g.vimtex_syntax_conceal_disable = 1
 vim.wo.conceallevel = 1
 
 vim.diagnostic.config({ virtual_text = true })
